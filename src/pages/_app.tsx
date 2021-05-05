5106fd6175d41659ebb87d2d2606e6f16bb143ee
edsonpaulo <edsonpaulo24@gmail.com>
2021-05-05 03:06:38 +0100

    feat: add chakraui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
-function EdsonPauloApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-export default EdsonPauloApp;
+const MainApp = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
+
+export default MainApp;
