e5c7ee23420e70fb4d962d5a6535d628b1569497
edsonpaulo <edsonpaulo24@gmail.com>
2021-05-11 00:54:35 +0100

    chore: next-i18next config deleted

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  i18n: {
-    defaultLocale: "pt",
-    locales: ["pt", "en"],
-    // localeDetection: false,
-    localePath: typeof window === "undefined" ? "public/locales" : "locales",
-  },
-};
